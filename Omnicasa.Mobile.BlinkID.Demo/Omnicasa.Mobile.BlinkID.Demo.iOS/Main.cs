95b44d7974bba4c2f2882e42c276bac623750c8c
HoangQuach <quachhoangqb@gmail.com>
2024-07-05 17:26:49 +0700

    MOBILEOMNI-8820 implement shared blink for Xamarin (#4)
    * Add shared project
    
    * Implement blink shared classes
    
    * Implement shared package for Xamarin.Forms
    
    * Update CI/CD for shared packages
    
    * Fix shared project build
    
    * Add  /t:Restore
    
    * Update Add /t:Restore for platform projects
    
    * Fix build shared platform
    
    * Test build
    
    * Complete build

---
+++ Omnicasa.Mobile.BlinkID.Demo/Omnicasa.Mobile.BlinkID.Demo.iOS/Main.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Main.cs" company="Omnicasa HoangQuach">
+// Copyright (c) Omnicasa HoangQuach. All rights reserved.
+// </copyright>
+
+#pragma warning disable SA1300
+namespace Omnicasa.Mobile.BlinkID.Demo.iOS
+#pragma warning restore SA1300
+{
+    using UIKit;
+
+#pragma warning disable S1118
+#pragma warning disable SA1600
+#pragma warning disable SA1649
+    public class Application
+#pragma warning restore SA1649
+#pragma warning restore SA1600
+#pragma warning restore S1118
+    {
+        // This is the main entry point of the application.
+        private static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, typeof(AppDelegate));
+        }
+    }
+}
---
+++ Omnicasa.Mobile.BlinkID.Shared/CardRecognizer.cs
@@ -0,0 +1,15 @@
+﻿namespace Omnicasa.Mobile.BlinkID.Shared
+{
+    /// <summary>CardRecognizer.</summary>
+    public class CardRecognizer
+    {
+        /// <summary>FirstName.</summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>LastName.</summary>
+        public string? LastName { get; set; }
+
+        /// <summary>FullName.</summary>
+        public string? FullName { get; set; }
+    }
+}
