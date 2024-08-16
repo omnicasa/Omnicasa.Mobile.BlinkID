47eb3a43cdd64bdfa11987c16b4cc0264844eb4e
HoangQuach <quachhoangqb@gmail.com>
2024-08-16 19:54:37 +0700

    Use Blink arm64 instead of mixing due issue on MacM3

--- Omnicasa.Mobile.BlinkID.iOS.Demo/AppDelegate.cs
+++ Omnicasa.Mobile.BlinkID.iOS.Demo/AppDelegate.cs
@@ -1,36 +1,41 @@
 ﻿using Foundation;
 using UIKit;
 
+#pragma warning disable SA1300
 namespace Omnicasa.Mobile.BlinkID.iOS.Demo
+#pragma warning restore SA1300
 {
+    /// <inheritdoc/>
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
-    [Register ("AppDelegate")]
-    public class AppDelegate : UIResponder, IUIApplicationDelegate {
-    
+    [Register("AppDelegate")]
+    public class AppDelegate : UIResponder, IUIApplicationDelegate
+    {
+        /// <inheritdoc/>
         [Export("window")]
         public UIWindow Window { get; set; }
 
-        [Export ("application:didFinishLaunchingWithOptions:")]
-        public bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+        /// <inheritdoc/>
+        [Export("application:didFinishLaunchingWithOptions:")]
+        public bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
             // Override point for customization after application launch.
             // If not required for your application you can safely delete this method
             return true;
         }
 
-        // UISceneSession Lifecycle
-
-        [Export ("application:configurationForConnectingSceneSession:options:")]
-        public UISceneConfiguration GetConfiguration (UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
+        /// <inheritdoc/>
+        [Export("application:configurationForConnectingSceneSession:options:")]
+        public UISceneConfiguration GetConfiguration(UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
         {
             // Called when a new scene session is being created.
             // Use this method to select a configuration to create the new scene with.
-            return UISceneConfiguration.Create ("Default Configuration", connectingSceneSession.Role);
+            return UISceneConfiguration.Create("Default Configuration", connectingSceneSession.Role);
         }
 
-        [Export ("application:didDiscardSceneSessions:")]
-        public void DidDiscardSceneSessions (UIApplication application, NSSet<UISceneSession> sceneSessions)
+        /// <inheritdoc/>
+        [Export("application:didDiscardSceneSessions:")]
+        public void DidDiscardSceneSessions(UIApplication application, NSSet<UISceneSession> sceneSessions)
         {
             // Called when the user discards a scene session.
             // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
@@ -38,5 +43,3 @@ namespace Omnicasa.Mobile.BlinkID.iOS.Demo
         }
     }
 }
-
-
--- Omnicasa.Mobile.BlinkID.iOS.Demo/ViewController.cs
+++ Omnicasa.Mobile.BlinkID.iOS.Demo/ViewController.cs
@@ -1,30 +1,37 @@
-﻿using CoreGraphics;
-using Foundation;
-using System;
+﻿using System;
+using CoreGraphics;
 using UIKit;
 
+#pragma warning disable SA1300
 namespace Omnicasa.Mobile.BlinkID.iOS.Demo
+#pragma warning restore SA1300
 {
-    interface IMBBlinkIdOverlayViewControllerDelegate
+    /// <inheritdoc/>
+    internal interface IMBBlinkIdOverlayViewControllerDelegate
     {
+        /// <inheritdoc/>
         void BlinkIdOverlayViewControllerDidFinishScanning(
             MBBlinkIdOverlayViewController blinkIdOverlayViewController,
             MBRecognizerResultState state);
 
+        /// <inheritdoc/>
         void BlinkIdOverlayViewControllerDidTapClose(
             MBBlinkIdOverlayViewController blinkIdOverlayViewController);
     }
 
-    class CustomMBBlinkIdOverlayViewControllerDelegate : MBBlinkIdOverlayViewControllerDelegate
+    /// <inheritdoc/>
+    internal class CustomMBBlinkIdOverlayViewControllerDelegate : MBBlinkIdOverlayViewControllerDelegate
     {
         private IMBBlinkIdOverlayViewControllerDelegate controllerDelegate;
 
+        /// <inheritdoc/>
         public CustomMBBlinkIdOverlayViewControllerDelegate(
             IMBBlinkIdOverlayViewControllerDelegate controllerDelegate)
         {
             this.controllerDelegate = controllerDelegate;
         }
 
+        /// <inheritdoc/>
         public override void BlinkIdOverlayViewControllerDidFinishScanning(
             MBBlinkIdOverlayViewController blinkIdOverlayViewController,
             MBRecognizerResultState state)
@@ -34,26 +41,33 @@ namespace Omnicasa.Mobile.BlinkID.iOS.Demo
                 state);
         }
 
+        /// <inheritdoc/>
         public override void BlinkIdOverlayViewControllerDidTapClose(MBBlinkIdOverlayViewController blinkIdOverlayViewController)
         {
             controllerDelegate.BlinkIdOverlayViewControllerDidTapClose(blinkIdOverlayViewController);
         }
     }
 
+    /// <inheritdoc/>
     public partial class ViewController : UIViewController, IMBBlinkIdOverlayViewControllerDelegate
     {
-        private MBBlinkIdMultiSideRecognizer mBBlinkIdMultiSideRecognizer;
+        private MBBlinkIdMultiSideRecognizer? mBBlinkIdMultiSideRecognizer;
+        private MBLicenseError? mBLicenseError;
+        private UIViewController? scannerViewController;
+
+        /// <inheritdoc/>
         public const string iOSLic = "sRwAAAETY29tLm9tbmljYXNhLm1vYmlsZXEPe6POZt4PSoCbv7EneOY6qMOcReFvL6VLejgXyGu/S7xlYbv6QgiyU/fYd8harXPQGCVH4xKMRD0blOjniQtx5Fv97rt7lrlNpr885nqSXcb83vXEjvxGkhLbN8VFIXCWV/GZpQonCwmVPTgs9jF9a2HX1pu3/mROCDKCQ5KiT5h8MRhMLyih2g2aXWKgtbQ0bcWU";
 
-        public ViewController (IntPtr handle) : base (handle)
+        /// <inheritdoc/>
+        public ViewController(IntPtr handle)
+            : base(handle)
         {
         }
 
-        public override void ViewDidLoad ()
+        /// <inheritdoc/>
+        public override void ViewDidLoad()
         {
-            base.ViewDidLoad ();
-            // Perform any additional setup after loading the view, typically from a nib.
-
+            base.ViewDidLoad();
             var but = new UIButton(new CGRect(100, 100, 100, 100))
             {
                 BackgroundColor = UIColor.Red,
@@ -61,14 +75,12 @@ namespace Omnicasa.Mobile.BlinkID.iOS.Demo
             };
             but.SetTitle("Start scan", UIControlState.Normal);
             but.TouchUpInside += But_TouchUpInside;
-            this.View.AddSubview(but);
+            View?.AddSubview(but);
 
             MBMicroblinkSDK.SharedInstance().SetLicenseKey(iOSLic, (MBLicenseError arg0) =>
             {
-                System.Diagnostics.Debug.WriteLine(arg0.ToString());
+                mBLicenseError = arg0;
             });
-
-            
         }
 
         private void But_TouchUpInside(object sender, EventArgs e)
@@ -79,28 +91,29 @@ namespace Omnicasa.Mobile.BlinkID.iOS.Demo
             MBBlinkIdOverlaySettings mBBlinkIdOverlaySettings = new MBBlinkIdOverlaySettings();
             MBRecognizerCollection mBRecognizerCollection = new MBRecognizerCollection(new[] { mBBlinkIdMultiSideRecognizer });
 
-
             MBBlinkIdOverlayViewController mBBlinkIdOverlayViewController = new MBBlinkIdOverlayViewController(
                 mBBlinkIdOverlaySettings,
                 mBRecognizerCollection,
-                new CustomMBBlinkIdOverlayViewControllerDelegate(this)
-                );
+                new CustomMBBlinkIdOverlayViewControllerDelegate(this));
 
             var recognizerRunneViewController = MBViewControllerFactory
                 .RecognizerRunnerViewControllerWithOverlayViewController(mBBlinkIdOverlayViewController);
 
-            this.PresentViewController(
-                ObjCRuntime.Runtime.GetINativeObject<UIViewController>(recognizerRunneViewController.Handle, false),
-                true,
-                null);
+            if (recognizerRunneViewController == null)
+            {
+                return;
+            }
+
+            scannerViewController = ObjCRuntime.Runtime.GetINativeObject<UIViewController>(recognizerRunneViewController.Handle, false);
+            if (scannerViewController == null)
+            {
+                return;
+            }
+
+            PresentViewController(scannerViewController, true, null);
         }
 
-        public override void DidReceiveMemoryWarning ()
-        {
-            base.DidReceiveMemoryWarning ();
-            // Release any cached data, images, etc that aren't in use.
-        }
-
+        /// <inheritdoc/>
         public void BlinkIdOverlayViewControllerDidFinishScanning(
             MBBlinkIdOverlayViewController blinkIdOverlayViewController,
             MBRecognizerResultState state)
@@ -108,120 +121,30 @@ namespace Omnicasa.Mobile.BlinkID.iOS.Demo
             System.Diagnostics.Debug.WriteLine($"DidFinishScanning with state={state}");
             if (state == MBRecognizerResultState.Valid)
             {
-                var result = mBBlinkIdMultiSideRecognizer.Result;
+                var result = mBBlinkIdMultiSideRecognizer?.Result;
                 if (result != null)
                 {
-                    System.Diagnostics.Debug.WriteLine(result.FirstName?.ToString() ?? "");
-                    System.Diagnostics.Debug.WriteLine(result.LastName?.ToString() ?? "");
+                    DismissScanController();
+                    System.Diagnostics.Debug.WriteLine(result.FirstName?.ToString() ?? string.Empty);
+                    System.Diagnostics.Debug.WriteLine(result.LastName?.ToString() ?? string.Empty);
                 }
             }
         }
 
+        /// <inheritdoc/>
         public void BlinkIdOverlayViewControllerDidTapClose(
             MBBlinkIdOverlayViewController blinkIdOverlayViewController)
         {
-        }
-    }
-}
-
-/*
-import UIKit
-import BlinkID
-
-class ViewController: UIViewController {
-
-    var blinkIdRecognizer: MBBlinkIdMultiSideRecognizer?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Valid until: 2024-10-10
-        MBMicroblinkSDK.shared().setLicenseResource("blinkid-license", withExtension: "txt", inSubdirectory: "", for: Bundle.main) { (_) in
-        }
-    }
-
-    @IBAction func didTapScan(_ sender: AnyObject) {
-
-self.blinkIdRecognizer = MBBlinkIdMultiSideRecognizer()
-        self.blinkIdRecognizer?.returnFullDocumentImage = true
-
-        let settings: MBBlinkIdOverlaySettings = MBBlinkIdOverlaySettings()
-
-        let recognizerList = [self.blinkIdRecognizer!]
-        let recognizerCollection: MBRecognizerCollection = MBRecognizerCollection(recognizers: recognizerList)
-
-        let blinkIdOverlayViewController: MBBlinkIdOverlayViewController =
-            MBBlinkIdOverlayViewController(settings: settings, recognizerCollection: recognizerCollection, delegate: self)
-
-        guard let recognizerRunneViewController: UIViewController =
-            MBViewControllerFactory.recognizerRunnerViewController(withOverlayViewController: blinkIdOverlayViewController) else
-{
-    return
-        }
-recognizerRunneViewController.modalPresentationStyle = .fullScreen
-
-        self.present(recognizerRunneViewController, animated: true, completion: nil)
-    }
-
-    @IBAction func didTapCustomUI(_ sender: Any) {
-
-    self.blinkIdRecognizer = MBBlinkIdMultiSideRecognizer()
-
-        let recognizerList = [self.blinkIdRecognizer!]
-        let recognizerCollection: MBRecognizerCollection = MBRecognizerCollection(recognizers: recognizerList)
-
-        let customOverlayViewController: CustomOverlay = CustomOverlay.initFromStoryboard()
-
-        customOverlayViewController.reconfigureRecognizers(recognizerCollection)
-
-        guard let recognizerRunneViewController: UIViewController =
-            MBViewControllerFactory.recognizerRunnerViewController(withOverlayViewController: customOverlayViewController) else
-    {
-        return
+            DismissScanController();
         }
 
-    recognizerRunneViewController.modalPresentationStyle = .fullScreen
-
-        self.present(recognizerRunneViewController, animated: true, completion: nil)
-    }
-}
-
-extension ViewController: MBBlinkIdOverlayViewControllerDelegate {
-
-    func blinkIdOverlayViewControllerDidFinishScanning(_ blinkIdOverlayViewController: MBBlinkIdOverlayViewController, state: MBRecognizerResultState) {
-    blinkIdOverlayViewController.recognizerRunnerViewController?.pauseScanning()
-
-        var message: String = ""
-        var title: String = ""
-        if self.blinkIdRecognizer?.result.resultState == MBRecognizerResultState.valid {
-        title = "BlinkID"
-
-            let fullDocumentImage: UIImage! = self.blinkIdRecognizer?.result.fullDocumentFrontImage?.image
-            print("Got BlinkID image with width: \(fullDocumentImage.size.width), height: \(fullDocumentImage.size.height)")
-
-            // Save the string representation of the code
-            message = self.blinkIdRecognizer!.result.description
-
-            DispatchQueue.main.async {
-            // present the alert view with scanned results
-
-            let alertController: UIAlertController = UIAlertController.init(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-
-                let okAction: UIAlertAction = UIAlertAction.init(title: "OK", style: UIAlertAction.Style.default,
-                                                                 handler: {
-                (_)->Void in
-                                                                    self.dismiss(animated: true, completion: nil)
-                })
-                alertController.addAction(okAction)
-                blinkIdOverlayViewController.present(alertController, animated: true, completion: nil)
+        /// <inheritdoc/>
+        private void DismissScanController()
+        {
+            if (scannerViewController != null)
+            {
+                scannerViewController.DismissViewController(true, null);
             }
+        }
     }
 }
-
-func blinkIdOverlayViewControllerDidTapClose(_ blinkIdOverlayViewController: MBBlinkIdOverlayViewController) {
-    self.dismiss(animated: true, completion: nil)
-    }
-}
-
-
- */
