e251c71e9bf09572d1bd7ab16d56f3fa0076dfb1
HoangQuach <quachhoangqb@gmail.com>
2025-04-29 10:08:36 +0700

    Revert to 5.19 for droid and update card parser from platform to shared (#14)
    * Revert to 5.19 for droid
    
    * Update metadata
    
    * Update card parser for droid

--- Omnicasa.Mobile.BlinkID.Maui.Demo/Platforms/Android/MainActivity.cs
+++ Omnicasa.Mobile.BlinkID.Maui.Demo/Platforms/Android/MainActivity.cs
@@ -1,11 +1,27 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Omnicasa.Mobile.BlinkID.Shared.Droid;
+using Omnicasa.Mobile.BlinkID.Shared.Maui;
+using Unity;
 
 namespace Omnicasa.Mobile.BlinkID.Maui.Demo;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        BlinkIDInitializer.Init(this, this);
+        App.Container.RegisterType<IBlinkIDServiceExtended, BlinkIDService>();
+    }
+
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+        BlinkIDHelper.OnActivityResult(requestCode, resultCode, data);
+    }
 }
 
